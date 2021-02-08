fbaf35f9243828f79922aae192c60dab986f74ca
Elijah <ilya.datsyuk@gmail.com>
2021-02-09 00:32:31 +0200

    refactor surroundings with respect to Direction model

--- src/main/scala/whchess/syntax/square.scala
+++ src/main/scala/whchess/syntax/square.scala
@@ -1,6 +1,7 @@
 package whchess.syntax
 
 import whchess.domain._
+import Direction._, Straight._, Diagonal._
 
 object square {
   implicit class SquareOps(square: Square) {
@@ -8,38 +9,28 @@ object square {
     def isOnSameColumn(that: Square): Boolean = square.file == that.file
     def isOnSameRow(that: Square):    Boolean = square.rank == that.rank
 
-    lazy val above:   Square = getSquareFor(same)(next)
-    lazy val below:   Square = getSquareFor(same)(previous)
-    lazy val toLeft:  Square = getSquareFor(previous)(same)
-    lazy val toRight: Square = getSquareFor(next)(same)
+    // straight ones
+    lazy val above:      Option[Square] = getSquareFor(same)(next)
+    lazy val below:      Option[Square] = getSquareFor(same)(previous)
+    lazy val toLeft:     Option[Square] = getSquareFor(previous)(same)
+    lazy val toRight:    Option[Square] = getSquareFor(next)(same)
 
-    lazy val surroundings: Vector[Square] = {
+    // diagonal ones
+    lazy val leftAbove:  Option[Square] = getSquareFor(previous)(next)
+    lazy val rightAbove: Option[Square] = getSquareFor(next)(next)
+    lazy val leftBelow:  Option[Square] = getSquareFor(previous)(previous)
+    lazy val rightBelow: Option[Square] = getSquareFor(next)(previous)
 
-      def filterSquaresThat(predicate: Square => Boolean): IndexedSeq[Square] =
-        Square.values.filter(predicate)
-
-      lazy val aboveRow    = filterSquaresThat(above.isOnSameRow)
-      lazy val belowRow    = filterSquaresThat(below.isOnSameRow)
-      lazy val leftColumn  = filterSquaresThat(toLeft.isOnSameColumn)
-      lazy val rightColumn = filterSquaresThat(toRight.isOnSameColumn)
-
-      val columns: IndexedSeq[Square] = square.file match {
-        case File.A => rightColumn
-        case File.H => leftColumn
-        case _      => leftColumn ++ rightColumn
-      }
-
-      val rows: IndexedSeq[Square] = square.rank match {
-        case Rank.`1` => aboveRow
-        case Rank.`8` => belowRow
-        case _        => aboveRow ++ belowRow
-      }
-
-      val straight = Vector(above, below, toLeft, toRight)
-      val diagonal = columns intersect rows
-
-      (straight ++ diagonal).filterNot(_ == square)
-    }
+    lazy val surroundings: Map[Direction, Option[Square]] = Direction.values.map {
+      case dir @ Up        => dir -> above
+      case dir @ Down      => dir -> below
+      case dir @ Left      => dir -> toLeft
+      case dir @ Right     => dir -> toRight
+      case dir @ UpLeft    => dir -> leftAbove
+      case dir @ UpRight   => dir -> rightAbove
+      case dir @ DownLeft  => dir -> leftBelow
+      case dir @ DownRight => dir -> rightBelow
+    }.toMap
 
     // private members
     private lazy val same:     Int => Int = identity[Int]
