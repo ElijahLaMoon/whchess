ce74c33164e27c70bec5d389b578a688c1f7519e
Elijah <ilya.datsyuk@gmail.com>
2021-02-10 12:38:29 +0200

    assign symbol to each PieceType

--- src/main/scala/whchess/domain/PieceType.scala
+++ src/main/scala/whchess/domain/PieceType.scala
@@ -2,16 +2,16 @@ package whchess.domain
 
 import enumeratum._
 
-sealed abstract class PieceType extends EnumEntry
+sealed abstract class PieceType(val symbol: Char) extends EnumEntry
 
 object PieceType extends Enum[PieceType] {
 
-  case object King   extends PieceType
-  case object Queen  extends PieceType
-  case object Bishop extends PieceType
-  case object Knight extends PieceType
-  case object Rook   extends PieceType
-  case object Pawn   extends PieceType
+  case object King   extends PieceType('K')
+  case object Queen  extends PieceType('Q')
+  case object Bishop extends PieceType('B')
+  case object Knight extends PieceType('N')
+  case object Rook   extends PieceType('R')
+  case object Pawn   extends PieceType('P')
 
   lazy val values: IndexedSeq[PieceType] = findValues
 }
