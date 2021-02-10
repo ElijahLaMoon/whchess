09f3d0474e2106d93220fb2fd40e8a3b4f7bf208
Elijah <ilya.datsyuk@gmail.com>
2021-02-10 12:36:32 +0200

    deepen the hierarchy to have a type-level distinction between colored pieces

--- src/main/scala/whchess/domain/Piece.scala
+++ src/main/scala/whchess/domain/Piece.scala
@@ -2,41 +2,51 @@ package whchess.domain
 
 import enumeratum._
 
-import Color._
 import PieceType._
 
 sealed abstract class Piece(val color: Color, val pType: PieceType) extends EnumEntry {
   lazy val symbol: Char = color match {
-    case White => pType.toString.head
-    case Black => pType.toString.head.toLower
+    case Color.White => pType.symbol
+    case Color.Black => pType.symbol.toLower
   }
 }
 
 object Piece extends Enum[Piece] {
 
-  lazy val whites: Map[Piece, Int] = pieces(White)
-  lazy val blacks: Map[Piece, Int] = pieces(Black)
+  lazy val whites: Map[White, Int] = pieces(White.values)
+  lazy val blacks: Map[Black, Int] = pieces(Black.values)
 
-  case object `White King`   extends Piece(White, King)
-  case object `Black King`   extends Piece(Black, King)
-  case object `White Queen`  extends Piece(White, Queen)
-  case object `Black Queen`  extends Piece(Black, Queen)
-  case object `White Bishop` extends Piece(White, Bishop)
-  case object `Black Bishop` extends Piece(Black, Bishop)
-  case object `White Knight` extends Piece(White, Knight)
-  case object `Black Knight` extends Piece(Black, Knight)
-  case object `White Rook`   extends Piece(White, Rook)
-  case object `Black Rook`   extends Piece(Black, Rook)
-  case object `White Pawn`   extends Piece(White, Pawn)
-  case object `Black Pawn`   extends Piece(Black, Pawn)
+  sealed abstract class White(override val pType: PieceType) extends Piece(Color.White, pType) with EnumEntry
+  object White extends Enum[White] {
+
+    case object `White King`   extends White(King)
+    case object `White Queen`  extends White(Queen)
+    case object `White Bishop` extends White(Bishop)
+    case object `White Knight` extends White(Knight)
+    case object `White Rook`   extends White(Rook)
+    case object `White Pawn`   extends White(Pawn)
+
+    lazy val values: IndexedSeq[White] = findValues
+  }
+
+  sealed abstract class Black(override val pType: PieceType) extends Piece(Color.Black, pType) with EnumEntry
+  object Black extends Enum[Black] {
+
+    case object `Black King`   extends Black(King)
+    case object `Black Queen`  extends Black(Queen)
+    case object `Black Bishop` extends Black(Bishop)
+    case object `Black Knight` extends Black(Knight)
+    case object `Black Rook`   extends Black(Rook)
+    case object `Black Pawn`   extends Black(Pawn)
+
+    lazy val values: IndexedSeq[Black] = findValues
+  }
 
   lazy val values: IndexedSeq[Piece] = findValues
 
   // private members
-  private def pieces(color: Color): Map[Piece, Int] = {
-    val filteredPieces = values.filter(_.color == color)
-
-    filteredPieces.map(p => p.pType match {
+  private def pieces[A <: Piece](enums: IndexedSeq[A]): Map[A, Int] = {
+    enums.map(p => p.pType match {
       case King   => p -> 1
       case Queen  => p -> 1
       case Bishop => p -> 2
